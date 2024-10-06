64e7a61838a2230d5645a6575cc71e990563c0b1
cerdowizard <holuwayinkzzy@gmail.com>
2024-10-07 00:34:21 +0100

    update struct with match statements

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -2,19 +2,22 @@ use std::io;
 
 #[derive(Debug)]
 enum AccountType {
-    Savings(String),
-    Current(String),
-    Local(String),
+    Savings,
+    Current,
+    Local,
 }
-impl AccountType {
-    fn new(&self) -> AccountType {
-        match self {
-            saving => AccountType::Savings("savings".to_string()),
-            current => AccountType::Current("current".to_string()),
-            local => AccountType::Local("local".to_owned()),
-        }
-    }
-}
+
+// impl AccountType {
+//     fn to_string(&self) -> String {
+//         match self {
+//         AccountType::Savings => "savings".to_string(),
+//         AccountType::Current => "current".to_string(),
+//         AccountType::Local => "local".to_string(),
+//         }
+//     }
+// }
+//
+#[warn(dead_code)]
 #[derive(Debug)]
 struct User {
     username: String,
@@ -24,50 +27,90 @@ struct User {
     account_type: AccountType,
 }
 
+// trait NewTrait {
+//     fn create_user(username: String, email: String, account_type:AccountType) -> Self;
+//     fn get_user(&self);
+//     fn update_user(&self, username: String, email: String, account_type:AccountType) -> User;
+//     fn deactivate_email(&mut self) -> bool;
+// }
+#[allow(dead_code)]
 impl User{
-    fn create_user(username: String, email: String) -> User {
+    fn create_user(username: String, email: String, account_type:AccountType) -> Self {
         User {
             username,
             email,
             sign_in_count: 1,
             active: true,
-            account_type:AccountType::Current("current".to_string()),
+            account_type
         }
     }
-    fn get_user(&self){
-        println!("{:?}, {:?}, {:?}, {:?}", self.username, self.email, self.sign_in_count, self.active)
-    }
 
-    fn update_user(&self, username: String, email: String) -> User {
+    fn get_user(&self){
+        println!("{:?}, {:?}, {:?}, {:?}, {:?}", self.username, self.email, self.sign_in_count, self.active, self.account_type)
+    }
+    fn update_user(&self, username: String, email: String, account_type:AccountType) -> User {
         User {
             username,
             email,
             sign_in_count: self.sign_in_count,
             active: self.active,
-            account_type: AccountType::Current("current".to_string())
+            account_type
         }
     }
-
     fn deactivate_email(&mut self) -> bool{
         self.active = false;
         self.active
     }
 }
-
+fn menu_list(){
+    println!(" >> Enter 1 to create an account");
+    println!(" >> Enter 2 to get account info");
+    println!(" >> Enter 3 to activate an account");
+    println!(" >> Enter 4 to send money");
+    println!(" >> Enter 5 to end application");
+}
 fn main() {
-    println!("Enter your name: ");
-    let mut user_name = String::new();
-    io::stdin().read_line(&mut user_name).expect("Failed to read line");
-    let trimed = user_name.trim();
+    loop {
+        menu_list();
+        let mut user_choice = String::new();
+        io::stdin().read_line(&mut user_choice).expect("Failed to read line");
 
-    println!("Enter your email: ");
-    let mut user_email = String::new();
-    io::stdin().read_line(&mut user_email).expect("Failed to read line");
-    let trimed_email = user_email.trim();
+        match user_choice.trim().parse() {
+             Ok(1) => {
+                println!("Enter your name: ");
+                let mut user_name = String::new();
+                io::stdin().read_line(&mut user_name).expect("Failed to read line");
+                let trimed = user_name.trim();
 
-    let user = User::create_user(trimed.to_string(), trimed_email.to_string());
-    let user_info = user.get_user();
-    let updated_user = user.update_user("dave".to_string(), "dave@gmail.com".to_string());
+                println!("Enter your email: ");
+                let mut user_email = String::new();
+                io::stdin().read_line(&mut user_email).expect("Failed to read line");
+                let trimed_email = user_email.trim();
 
-    println!("{:?}", updated_user);
+                println!("Enter your account type (savings/current/local): ");
+                let mut user_account_type = String::new();
+                io::stdin().read_line(&mut user_account_type).expect("Failed to read user input");
+
+                let account_type = match user_account_type.trim() {
+                    "savings" => AccountType::Savings,
+                    "current" => AccountType::Current,
+                    "local" => AccountType::Local,
+                    _ => {
+                        println!("Invalid account type. Defaulting to Current.");
+                        AccountType::Current
+                    }
+                };
+
+                let user = User::create_user(trimed.to_string(), trimed_email.to_string(), account_type);
+            }
+
+            Ok(5) => {
+                println!("closing application");
+                break;
+            }
+            _ => println!("Invalid choice. Please try again."),
+        };
+
+        // user.get_user();
+    }
 }
